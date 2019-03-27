4a335159bbbf25a44529c8e002d08868473ffe6a
financial <financialfly@outlook.com>
2019-03-27 14:58:48 +0800

    更新http和https分开

--- proxy_pool/getter.py
+++ proxy_pool/getter.py
@@ -1,8 +1,8 @@
 import re
 
-from lxml import etree
 from .utils import get_html
 from .proxy import Proxy
+from .db import formproxy
 
 class ProxyGetter(Proxy):
 
@@ -11,50 +11,54 @@ class ProxyGetter(Proxy):
         self.crawlers = [x for x in dir(self) if 'crawl' in x]
 
     def crawl_kuaidaili(self):
+        '''快代理'''
         url = 'https://www.kuaidaili.com/free/inha/1/'
         r = get_html(url)
         if r:
-            ip_adress = re.compile(
-                '<td data-title="IP">(.*)</td>\s*<td data-title="PORT">(\w+)</td>'
-            )
-            re_ip_adress = ip_adress.findall(r.text)
-            for adress, port in re_ip_adress:
+            ip = re.compile(r'td data-title="IP">(.*?)</td>\s*<td data-title="PORT">(\w+)</td>\s*<td data-title="匿名度">(.*?)</td>\s*<td data-title="类型">(.*?)</td>')
+            iplist = ip.findall(r.text)
+            for adress, port, anonymity, iptype in iplist:
+                if not anonymity == '高匿名':
+                    continue
                 result = adress + ':' + port
-                yield result.replace(' ', '')
+                yield formproxy(iptype, result)
 
     def crawl_data5u(self):
+        '''无忧代理'''
         url = 'http://www.data5u.com/free/gngn/index.shtml'
         r = get_html(url)
-        ip_adress = re.compile(
-            ' <ul class="l2">\s*<span><li>(.*?)</li></span>\s*<span style="width: 100px;"><li class=".*">(.*?)</li></span>'
-        )
-        # \s * 匹配空格，起到换行作用
-        re_ip_adress = ip_adress.findall(r.text)
-        for adress, port in re_ip_adress:
+        ip = re.compile(r'<span><li>(.*?)</li></span>\s*<span.*?class="port GEGEA">(\w+)</li></span>\s*<span.*?><li>(.*?)</li></span>\s*<span.*?><li>(.*?)</li></span>')
+        iplist = ip.findall(r.text)
+        for adress, port, anonymity, iptype in iplist:
+            if not anonymity == '高匿':
+                continue
             result = adress + ':' + port
-            yield result.replace(' ', '')
+            yield formproxy(iptype, result)
 
     def crawl_ip3366(self):
+        '''云代理'''
         url = 'http://www.ip3366.net/free/?stype=1&page=1'
         r = get_html(url)
-        html = etree.HTML(r.text)
-        trs = html.xpath('//table[@class="table table-bordered table-striped"]/tbody/tr')
-        for tr in trs:
-            ip_addr = tr.xpath('.//td[1]/text()')[0]
-            ip_port = tr.xpath('.//td[2]/text()')[0]
-            full_ip = ip_addr + ':' + ip_port
-            yield full_ip.replace(' ','')
+        ip = re.compile(r'<td>(.*?)</td>\s*<td>(\w+)</td>\s*<td>(.*?)</td>\s*<td>(\w+)</td>')
+        iplist = ip.findall(r.content.decode('gb2312'))
+        for addr, port, anonymity, iptype in iplist:
+            if not anonymity.startswith('高匿'):
+                continue
+            result = '%s:%s' %(addr, port)
+            yield formproxy(iptype, result)
 
     def crawl_qydaili(self):
         '''旗云代理'''
         urls = ['http://www.qydaili.com/free/?action=china&page=%d' % x for x in range(1, 5)]
         for url in urls:
             r = get_html(url)
-            addrs = re.findall(r'data-title="IP">(.*?)</td>', r.text)
-            ports = re.findall(r'data-title="PORT">(\d+)</td>', r.text)
-            for a, p in zip(addrs, ports):
-                proxy = '%s:%s' %(a, p)
-                yield proxy
+            ip = re.compile(r'<td data-title="IP">(.*?)</td>\s*<td data-title="PORT">(\w+)</td>\s*<td data-title="匿名度">(.*?)</td>\s*<td data-title="类型">(.*?)</td>')
+            iplist = ip.findall(r.text)
+            for addr, port, anonymity, iptype in iplist:
+                if not anonymity == '高匿':
+                    continue
+                result = '%s:%s' %(addr, port)
+                yield formproxy(iptype, result)
 
 def crawl_funcs():
     '''获取爬虫函数'''
