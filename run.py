5dc5728c7c4dcaffd6ba74c8fc55acb9d85cb2dd
financial <financialfly@outlook.com>
2019-03-27 15:03:49 +0800

    update

--- run.py
+++ run.py
@@ -1,14 +1,15 @@
 from proxy_pool import get, check, run_web
 from multiprocessing import Process
 
-pros = list()
-funcs = [get, check, run_web]
+if __name__ == '__main__':
+    pros = list()
+    funcs = [get, check, run_web]
 
-for f in funcs:
-    p = Process(target=f)
-    pros.append(p)
-    p.start()
+    for f in funcs:
+        p = Process(target=f)
+        pros.append(p)
+        p.start()
 
-for p in pros:
-    p.join()
-run_web()
+    for p in pros:
+        p.join()
+    run_web()
