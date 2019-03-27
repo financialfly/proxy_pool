d5cf3e6f0e61ec6c5c8a8b73736caee854dd982d
financial <financialfly@outlook.com>
2019-03-27 15:01:09 +0800

    去除__main__

--- run.py
+++ run.py
@@ -1,16 +1,14 @@
 from proxy_pool import get, check, run_web
 from multiprocessing import Process
 
+pros = list()
+funcs = [get, check, run_web]
 
+for f in funcs:
+    p = Process(target=f)
+    pros.append(p)
+    p.start()
 
-if __name__ == '__main__':
-    pros = list()
-    funcs = [get, check, run_web]
-
-    for f in funcs:
-        p = Process(target=f)
-        pros.append(p)
-        p.start()
-
-    for p in pros:
-        p.join()
+for p in pros:
+    p.join()
+run_web()
