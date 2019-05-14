ade19dbf6fb3918fc7960642f85fd460d2962b47
financial <financialfly@outlook.com>
2019-05-14 11:53:46 +0800

    del crawl_funcs method and add routes method

--- proxy_pool/getter.py
+++ proxy_pool/getter.py
@@ -6,13 +6,22 @@ from .db import formproxy
 
 class ProxyGetter(Proxy):
 
-    def __init__(self):
-        # 获取所有爬虫函数，此时crawlers还没有被创建所以获取不到它本身
-        self.crawlers = [x for x in dir(self) if 'crawl' in x]
+    def routes(self):
+        routes = [
+            ('https://www.kuaidaili.com/free/inha/1/', self.crawl_kuaidaili),
+            ('http://www.data5u.com/free/gngn/index.shtml', self.crawl_data5u),
+            ('http://www.ip3366.net/free/?stype=1&page=1', self.crawl_ip3366),
+            (['http://www.qydaili.com/free/?action=china&page=%d' % x for x in range(1, 5)], self.crawl_qydaili)
+        ]
+        return routes
 
-    def crawl_kuaidaili(self):
+    @classmethod
+    def get_crawl_routes(cls):
+        g = cls()
+        return g.routes()
+
+    def crawl_kuaidaili(self, url):
         '''快代理'''
-        url = 'https://www.kuaidaili.com/free/inha/1/'
         r = get_html(url)
         if r:
             ip = re.compile(r'td data-title="IP">(.*?)</td>\s*<td data-title="PORT">(\w+)</td>\s*<td data-title="匿名度">(.*?)</td>\s*<td data-title="类型">(.*?)</td>')
@@ -23,21 +32,20 @@ class ProxyGetter(Proxy):
                 result = adress + ':' + port
                 yield formproxy(iptype, result)
 
-    def crawl_data5u(self):
+    def crawl_data5u(self, url):
         '''无忧代理'''
-        url = 'http://www.data5u.com/free/gngn/index.shtml'
         r = get_html(url)
-        ip = re.compile(r'<span><li>(.*?)</li></span>\s*<span.*?class="port GEGEA">(\w+)</li></span>\s*<span.*?><li>(.*?)</li></span>\s*<span.*?><li>(.*?)</li></span>')
+        ip = re.compile(r'<span><li>(.*?)</li></span>\s*' # 地址
+                        r'<span style="width: 100px;"><li class="port \w+">(\d+)</li></span>\s*' # 端口
+                        r'<span style="width: 100px; color:red;"><li>高匿</li></span>\s*' # 匿名
+                        r'<span style="width: 100px;"><li>(\w+)</li></span>') # 种类
         iplist = ip.findall(r.text)
-        for adress, port, anonymity, iptype in iplist:
-            if not anonymity == '高匿':
-                continue
+        for adress, port, iptype in iplist:
             result = adress + ':' + port
             yield formproxy(iptype, result)
 
-    def crawl_ip3366(self):
+    def crawl_ip3366(self, url):
         '''云代理'''
-        url = 'http://www.ip3366.net/free/?stype=1&page=1'
         r = get_html(url)
         if r:
             ip = re.compile(r'<td>(.*?)</td>\s*<td>(\w+)</td>\s*<td>(.*?)</td>\s*<td>(\w+)</td>')
@@ -48,9 +56,8 @@ class ProxyGetter(Proxy):
                 result = '%s:%s' %(addr, port)
                 yield formproxy(iptype, result)
 
-    def crawl_qydaili(self):
+    def crawl_qydaili(self, urls):
         '''旗云代理'''
-        urls = ['http://www.qydaili.com/free/?action=china&page=%d' % x for x in range(1, 5)]
         for url in urls:
             r = get_html(url)
             if r:
@@ -61,16 +68,3 @@ class ProxyGetter(Proxy):
                         continue
                     result = '%s:%s' %(addr, port)
                     yield formproxy(iptype, result)
-
-    # @classmethod
-    # def get_funcs(cls):
-    # '''获取爬虫函数'''
-    #     g = cls()
-    #     funcs =  [x for x in dir(g) if 'crawl' in x]
-    #     return [getattr(g, f) for f in funcs]
-
-def crawl_funcs():
-    '''获取爬虫函数'''
-    g = ProxyGetter()
-    funcs = [getattr(g, f) for f in g.crawlers]
-    return funcs
