64bbbea5a4eac6c3097cfedb5158bbea2acb30e4
financial <financialfly@outlook.com>
2019-05-14 11:54:23 +0800

    change func to route

--- proxy_pool/proxy.py
+++ proxy_pool/proxy.py
@@ -6,24 +6,23 @@ from .db import ProxySql
 
 class Proxy(object):
     '''基类'''
+    sql = ProxySql()
+
     @property
     def logger(self):
         return get_logger(self.__class__.__name__.lower().replace('proxy', ''))
 
-    @property
-    def sql(self):
-        return ProxySql()
-
 from threading import Thread
 class ProxyCrawler(Thread, Proxy):
     '''用于封装代理爬虫的多线程类'''
-    def __init__(self, crawl_func, *args, **kwargs):
-        self.func = crawl_func
+    def __init__(self, crawler_route, *args, **kwargs):
+        # ex. ('https://www.kuaidaili.com/free/inha/1/', self.crawl_kuaidaili)
+        self.url, self.func = crawler_route
         self.proxies = list()
         super().__init__(*args, **kwargs)
 
     def run(self):
-        for proxy in self.func():
+        for proxy in self.func(self.url):
             self.logger.debug('Got raw proxy %s' % proxy)
             self.proxies.append(proxy)
         self.logger.info('got proxies %s' % self.proxies)
