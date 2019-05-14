6af0c87498e2ecd2824ccff2d47cf37e32a94fba
financial <financialfly@outlook.com>
2019-05-14 11:51:39 +0800

    update class method

--- proxy_pool/__init__.py
+++ proxy_pool/__init__.py
@@ -9,12 +9,14 @@ from .scheduler import ProxyScheduler
 from .web import ProxyWebApi
 
 def get(interval=5):
-    s = ProxyScheduler()
-    return s.get_proxy(interval)
+    return ProxyScheduler.get_proxy(interval)
+    # s = ProxyScheduler()
+    # return s.get_proxy(interval)
 
 def check(interval=5):
-    s = ProxyScheduler()
-    return s.check_proxy(interval)
+    # s = ProxyScheduler()
+    # return s.check_proxy(interval)
+    return ProxyScheduler.check_proxy(interval)
 
 def run_web():
     a = ProxyWebApi()
