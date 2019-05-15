9a21091a01487e5b4c26b4eabb86801d5ab874dc
Hifinancial <financial@github.com>
2019-05-16 00:00:33 +0800

    update LOWER_LIMIT to 20

--- proxypool/settings.py
+++ proxypool/settings.py
@@ -3,15 +3,15 @@ HOST = 'localhost'
 PORT = 3306
 USER = 'root'
 PASSWORD = '1234'
-DATABASE = 'spider'
+DATABASE = 'proxies'
 TABLE = 'proxies'
 
 # 代理池设置
-LOWER_LIMIT = 100
-UPPER_LIMIT = 200
+LOWER_LIMIT = 20
+UPPER_LIMIT = 100
 
 # 检测间隔时间
-INTERVAL_TIME = 10
+INTERVAL_TIME = 15
 
 REDIS_HOST = 'localhost'
 REDIS_PORT = 6379
