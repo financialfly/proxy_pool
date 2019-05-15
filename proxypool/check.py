00b409d7fb161ef85aad6713c8b4ef5c19be8774
Hifinancial <financial@github.com>
2019-05-15 23:56:44 +0800

    update http check rules

--- proxypool/check.py
+++ proxypool/check.py
@@ -1,7 +1,7 @@
 import re
 
 from .db import DbClient
-from .utils import get_logger
+from .logger import get_logger
 from .request import Request, Crawler
 
 logger = get_logger('Checker')
@@ -13,12 +13,20 @@ class ProxyChecker(object):
         self.httpsurl = 'https://httpbin.org/ip'
         self.httpurl = 'http://2019.ip138.com/ic.asp'
         self.crawler = Crawler(requests=list(), logger=logger)
-        self.sql = DbClient()
+        self.db = DbClient()
 
     def check_http(self, response):
         proxy = response.meta['proxy']
-        if response.status_code == 200 and response.text:
-            current_ip = re.search(r'您的IP是：\[(.*?)\]', response.content.decode('gb2312')).group(1)
+        if response.content:
+            try:
+                text = response.content.decode('gb2312')
+            except UnicodeDecodeError:
+                text = None
+        else:
+            text = None
+        # Maximum number of open connections reached.
+        if text and not 'Maximum number' in text:
+            current_ip = re.search(r'您的IP是：\[(.*?)\]', text).group(1)
             logger.debug('Current ip is %s' % current_ip)
             self.process_result(proxy)
         else:
@@ -36,11 +44,11 @@ class ProxyChecker(object):
     def process_result(self, proxy, is_useful=True):
        '''处理检查的结果'''
        if is_useful:
-           self.sql.update_useful(proxy)
+           self.db.update_useful(proxy)
            logger.info('Got valid proxy %s' % proxy)
        else:
            logger.debug('invalid proxy %s' % proxy)
-           self.sql.delete(proxy)
+           self.db.delete(proxy)
 
     def check(self, count=10):
         self._update_reqs(iptype='http', count=count)
@@ -55,7 +63,7 @@ class ProxyChecker(object):
         else:
             url = self.httpsurl
             callback = self.check_https
-        http_proxies = self.sql.get_raw(count=count, iptype=iptype)
+        http_proxies = self.db.get_raw(count=count, iptype=iptype)
         if http_proxies:
             http_reqs = [Request(url=url,
                                  callback=callback,
