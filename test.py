26cca991f02bde188d3e795063646c15772ecb1d
financial <financialfly@outlook.com>
2019-05-17 11:27:50 +0800

    update

--- test.py
+++
@@ -1,7 +0,0 @@
-from proxypool.db import DbClient
-from proxypool.proxy import formproxy
-
-if __name__ == '__main__':
-    s = DbClient()
-    s.put(formproxy('http', '12347.122.45'))
-    print(s.get_raw())
